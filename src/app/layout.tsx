12b6b33691dbef4cf25ebae8a0020ccb83287aa8
garriagus <agustin_g08@hotmail.com>
2024-11-08 09:47:31 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased background-image backdrop-blur`}
       >
         {children}
       </body>
